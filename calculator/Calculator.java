31c66b09f59b76b77f8feb641ab6b6f378bbf0e4
Jihane Salek <jihane.salek63@gmail.com>
2024-12-11 20:26:48 +0100

    initial commit for  calculator project

---
+++ calculator/Calculator.java
@@ -0,0 +1,9 @@
+package com.jihane.calculator;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+
---
+++ calculator/Division.java
@@ -0,0 +1,11 @@
+package com.jihane.calculator;
+
+public class Division  {
+    public int devide(int a, int b) {
+        if (b==0){
+            System.out.println("impossible");
+        }
+        return a / b ;
+    }
+
+}
